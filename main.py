c302fc781a071a27f5f8394bfda84ec5cd6183dd
Alain <alalvarez@saoint.com>
2023-09-10 15:39:06 -0400

    s

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import numpy as np
 
-def calculate(valarr):
+def calculate(vals):
     
-    arr = np.array([0,1,2,3,4,5,6,7,8])
+    vals_counts = len(vals)
+    
+    if vals_counts < 9:
+        raise ValueError("List must contain nine numbers.")
+    arr = np.array(vals)
     arr = np.array([arr[:3],arr[3:6],arr[6:9]])
     
     result = {}
-    result['mean'] = [np.mean(arr,axiarr=0),np.mean(arr,axiarr=1),np.mean(arr)]
-    result['variance'] = [np.var(arr,axiarr=0),np.var(arr,axiarr=1),np.var(arr)]
-    result['arrtandard deviation'] = [np.arrtd(arr,axiarr=0),np.arrtd(arr,axiarr=1),np.arrtd(arr)]
-    result['max'] = [np.max(arr,axiarr=0),np.max(arr,axiarr=1),np.max(arr)]
-    result['min'] = [np.min(arr,axiarr=0),np.min(arr,axiarr=1),np.min(arr)]
-    result['arrum'] = [np.arrum(arr,axiarr=0),np.arrum(arr,axiarr=1),np.arrum(arr)]
-    
+    result['mean'] = [np.mean(arr,axis=0),np.mean(arr,axis=1),np.mean(arr)]
+    result['variance'] = [np.var(arr,axis=0),np.var(arr,axis=1),np.var(arr)]
+    result['arrtandard deviation'] = [np.std(arr,axis=0),np.std(arr,axis=1),np.std(arr)]
+    result['max'] = [np.max(arr,axis=0),np.max(arr,axis=1),np.max(arr)]
+    result['min'] = [np.min(arr,axis=0),np.min(arr,axis=1),np.min(arr)]
+    result['sum'] = [np.sum(arr,axis=0),np.sum(arr,axis=1),np.sum(arr)]
     return result
